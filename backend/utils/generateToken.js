9fa1edd82af1feec8a16b2191c830e166001047c
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-08-07 12:57:29 +0530

    Add: Generate token and set cookie

---
+++ backend/utils/generateToken.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken'
+
+const generateToken = (res, userId) => {
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+        expiresIn: '1d',
+    });
+
+    res.cookie('jwt', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'strict',
+        maxAge: 24 * 60 * 60 * 1000
+
+    })
+
+
+}
+
+export default generateToken;
