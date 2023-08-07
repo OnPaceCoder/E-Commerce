2b086e9ef33dac597861c691e281ad9a5c6ae5a7
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-08-07 14:07:01 +0530

    Add: Check isAdmin

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -29,5 +29,15 @@ const protect = async (req, res, next) => {
 
 }
 
+//User must be an admin 
 
+const admin = (req, res, next) => {
+    if (req.user && req.user.isAdmin) {
+        next();
+    } else {
+        res.status(401);
+        throw new Error("Not authorized as an admin")
+    }
+}
 
+export { protect, admin };
