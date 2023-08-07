2e85c0f8ff09b1e9a9d07c18696a88823cc40134
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-08-07 14:03:28 +0530

    Fix: Token decode issue

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -13,7 +13,9 @@ const protect = async (req, res, next) => {
 
     if (token) {
         try {
-            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            req.user = await User.findById(decoded.userId).select('-password');
+            next();
         } catch (error) {
             res.status(401);
             throw new Error('Not authorized , token failed')
@@ -26,3 +28,6 @@ const protect = async (req, res, next) => {
 
 
 }
+
+
+
