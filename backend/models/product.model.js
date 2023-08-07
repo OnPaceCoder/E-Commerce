7161746586cc19a7d3e09eb25a13899b638de939
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-08-07 12:46:03 +0530

    Add: Product model

---
+++ backend/models/product.model.js
@@ -0,0 +1,69 @@
+import mongoose from "mongoose";
+
+//Review Schema created using mongoose schema
+const reviewSchema = mongoose.Schema({
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comment: {
+        type: String, required: true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    }
+})
+
+//Product Schema created using mongoose schema
+
+const productSchema = mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User',
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    brand: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    review: [reviewSchema],
+    rating: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    numReviews: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    countInStock: {
+        type: Number,
+        required: true,
+        default: 0,
+    }
+
+}, {
+    timestamps: true,
+})
+
+
+//Product model created using mongoose model
+const Product = mongoose.model('Product', productSchema);
+export default Product
