def78b50be987f47d9c1312a88a623602ce2290e
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-08-07 11:31:47 +0530

    config: database

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 import express from "express";
+import dotenv from 'dotenv';
+import connectDB from "./config/db.js";
+dotenv.config()
 
 const app = express();
 
 
+connectDB()
 
-const PORT = 5001;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`)
