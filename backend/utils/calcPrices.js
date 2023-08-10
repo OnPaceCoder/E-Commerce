e987e1329a449d8b6017187016d7e83219ce0603
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-08-10 16:58:17 +0530

    feat: Calulate Price

---
+++ backend/utils/calcPrices.js
@@ -0,0 +1,29 @@
+function addDecimals(num) {
+    return (Math.round(num * 100) / 100).toFixed(2);
+}
+
+
+export function calcPrices(orderItems) {
+    //Calculate the items price
+    const itemsPrice = addDecimals(
+        orderItems.reduce((add, items) => acc + items.price * items.qty, 0))
+
+    //Calculate the shipping price
+    const shippingPrice = addDecimals(itemsPrice > 100 ? 0 : 10);
+
+    //Calculate the tax price
+    const taxPrice = addDecimals(Number((0.15 * itemsPrice).toFixed(2)))
+
+    //Calculate the total price
+
+    const totalPrice = (Number(itemsPrice) + Number(shippingPrice) + Number(taxPrice)).toFixed(2);
+
+
+    return { itemsPrice, shippingPrice, taxPrice, totalPrice };
+
+
+
+
+
+}
+
