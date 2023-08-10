2d83c5d7e90b8191a95a85cd57ca29a121604689
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-08-10 19:30:03 +0530

    Add: UserRoute

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,27 @@
 import express from "express";
 import dotenv from 'dotenv';
 import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
+import { errorHandler, notFound } from "./middleware/error.middleware.js";
 dotenv.config()
-
-const app = express();
+import userRoutes from './routes/user.routes.js'
+const PORT = process.env.PORT;
 
 
 connectDB()
 
-const PORT = process.env.PORT;
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use('/api/users', userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
 
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`)
