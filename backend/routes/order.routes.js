3cffa7a1f86575ae131894b6bbe0c0473e46cddd
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-08-13 23:51:43 +0530

    Add: Order routes

---
+++ backend/routes/order.routes.js
@@ -0,0 +1,27 @@
+import express from 'express';
+const router = express.Router();
+
+import {
+    addOrderItems,
+    getMyOrders,
+    getOrdersById,
+    updateOrderToPaid,
+    updateOrderToDelivered,
+    getOrders,
+} from '../controllers/order.controller.js'
+
+
+
+import { protect, admin } from '../middleware/auth.middleware.js'
+
+router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders);
+
+router.route('/mine').get(protect, getMyOrders);
+
+router.route('/:id').get(protect, getOrdersById);
+
+router.route('/:id/pay').put(protect, updateOrderToPaid);
+
+router.route('/:id/deliver').put(protect, admin, updateOrderToDelivered);
+
+export default router
