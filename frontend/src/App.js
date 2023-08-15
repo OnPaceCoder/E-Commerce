9dee3d5448834636d634eaabf6525b182d8fe2dc
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-08-15 22:45:00 +0530

    Add: layout setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,19 @@ import React from 'react'
 import { ToastContainer } from 'react-toastify'
 import Header from './components/Header.jsx'
 import { Outlet } from 'react-router-dom'
+import Footer from './components/Footer.jsx'
+import 'react-toastify/dist/ReactToastify.css';
 const App = () => {
   return (
     <>
       <ToastContainer />
 
       <Header />
-      <div className='py-24'>
-        <div className='container' >
-
-          <Outlet />
-        </div>
+      <div className='pt-12'>
+        <Outlet />
       </div>
 
+      <Footer />
 
 
     </>
