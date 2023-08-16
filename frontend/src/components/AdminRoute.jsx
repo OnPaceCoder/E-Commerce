6eed3583bb25a144e049d242ae162bfe0a956a48
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-08-16 23:38:14 +0530

    Add: Layout

--- frontend/src/components/AdminRoute.jsx
+++ frontend/src/components/AdminRoute.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { Navigate, Outlet } from 'react-router-dom';
 
 const AdminRoute = () => {
-    return (
-        <div>AdminRoute</div>
+    const { userInfo } = useSelector((state) => state.auth);
+    return userInfo && userInfo.isAdmin ? (<Outlet />) : (
+        <Navigate to='/login' replace />
     )
 }
 
