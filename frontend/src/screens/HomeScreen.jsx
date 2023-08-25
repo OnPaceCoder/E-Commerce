8067de9fb4226c06e2f31f7a72a7d4c30bf87b8d
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-08-25 23:20:23 +0530

    Add: HomeScreen updated

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
 import '../index.css'
-
+import { useGetProductsQuery } from '../slices/productsApiSlice'
+import { Link, useParams } from 'react-router-dom';
+import Loader from '../components/Loader';
+import { toast } from 'react-toastify';
 
 const HomeScreen = () => {
+
+    const { pageNumber, keyword } = useParams();
+    const { data, isLoading, error } = useGetProductsQuery({
+        keyword, pageNumber
+    })
+
     return (
         <div>
-            <h2 className="text-4xl text-blue-500">HomeScreen</h2>
+            {!keyword ? (<>Produce</>) : (<Link to="/" ><button className='px-3 py-2 bg-slate-500 rounded mx-2'>Go Back</button></Link>)}
+
+            {isLoading ? (<Loader />) : error ? (toast.error(`Error: ${error?.data?.message || error.error}`, { position: toast.POSITION.TOP_RIGHT })) :
+                (<>
+                    <div className='container'>
+                        <h1>Latest Products</h1>
+                        {data.products.map((product) => {
+                            // console.log(product)
+                        })}
+                    </div>
+
+                </>)}
+
+
+
         </div>
     )
 }
