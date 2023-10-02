c7ba9262c23f59314b2b11e919c1d0a841d7ee33
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-10-02 23:15:39 +0530

    Refactor: UI fixed

--- frontend/src/components/SearchBox.jsx
+++ frontend/src/components/SearchBox.jsx
@@ -22,7 +22,7 @@ const SearchBox = () => {
 
     return (
         <div>
-            <form action="" className='flex gap-4' onSubmit={submitHandler}>
+            <form action="" className='flex gap-2' onSubmit={submitHandler}>
                 <input className='bg-gra-200 px-4 py-2 border-gray-400 border rounded-md' type="text" placeholder='Search Products..' onChange={(e) => setKeyword(e.target.value)} />
                 <button className='px-4 py-1 bg-slate-200 rounded-lg text-lg'>Search</button>
             </form>
