638df09c21aef2d4bd55094a2731b6b767c79b1e
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-10-02 23:09:25 +0530

    fix: Layout updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,18 @@ import Header from './components/Header.jsx'
 import { Outlet } from 'react-router-dom'
 import Footer from './components/Footer.jsx'
 import 'react-toastify/dist/ReactToastify.css';
+import { Container } from 'react-bootstrap'
 const App = () => {
   return (
     <>
       <ToastContainer />
 
       <Header />
-      <div className='pt-12'>
+
+      <Container>
         <Outlet />
-      </div>
+      </Container>
+
 
       <Footer />
 
