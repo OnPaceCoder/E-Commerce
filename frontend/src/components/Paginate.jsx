5a313db33ef31fa4a6f5a85d5a324f07fc9135fc
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-10-02 23:17:38 +0530

    Add: Paginate

--- frontend/src/components/Paginate.jsx
+++ frontend/src/components/Paginate.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
-
-const Paginate = () => {
+import { Pagination } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+const Paginate = ({ pages, page, isAdmin = false, keyword = '' }) => {
     return (
-        <div>Paginate</div>
+        pages > 1 && (
+            <Pagination>
+                {
+                    [...Array(pages).keys()].map((x) => (
+                        <LinkContainer key={x + 1}
+
+                            to={
+                                !isAdmin
+                                    ? keyword
+                                        ? `/search/${keyword}/page/${x + 1}`
+                                        : `/page/${x + 1}`
+                                    : `/admin/productlist/${x + 1}`
+                            }>
+
+                            <Pagination.Item active={x + 1 === page}>{x + 1}</Pagination.Item>
+                        </LinkContainer>
+                    ))
+                }
+            </Pagination>
+        )
     )
 }
 
