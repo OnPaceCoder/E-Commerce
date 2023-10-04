7db4194b1ad3a715b180d8ef8859635958713019
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-10-04 19:06:24 +0530

    fix: Credentials included to send cookies

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -2,11 +2,16 @@ import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
 import { BASE_URL } from '../constants'
 
-const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
+
+const baseQuery = fetchBaseQuery({
+    baseUrl: BASE_URL,
+    credentials: 'include', // Include credentials in all requests
+});
 
 
 export const apiSlice = createApi({
     baseQuery,
     tagTypes: ['Product', 'Order', 'User'],
-    endpoints: (builder) => ({})
+
+    endpoints: (builder) => ({}),
 })
