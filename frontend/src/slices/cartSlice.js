4cb09e7a25f8d9f6f521f75e57794fab9df3a856
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-10-05 20:55:03 +0530

    fix: id to _id in find method

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -13,7 +13,7 @@ const cartSlice = createSlice({
         addToCart: (state, action) => {
             const item = action.payload;
 
-            const existItem = state.cartItems.find((x) => x.id === item._id)
+            const existItem = state.cartItems.find((x) => x._id === item._id)
 
             if (existItem) {
                 state.cartItems = state.cartItems.map((x) =>
