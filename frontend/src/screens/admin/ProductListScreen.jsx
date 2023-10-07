3bef3adf495a63f539761ff00763d0af507b1584
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-10-07 18:34:00 +0530

    Add: ProductList Screen

--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -1,8 +1,100 @@
 import React from 'react'
-
+import { useCreateProductMutation, useDeleteProductMutation, useGetProductsQuery } from '../../slices/productsApiSlice';
+import { toast } from 'react-toastify'
+import { useParams } from 'react-router-dom'
+import Paginate from '../../components/Paginate';
+import { Button, Col, Row, Table } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { FaEdit, FaPlus, FaTrash } from 'react-icons/fa'
+import Message from '../../components/Message';
+import Loader from '../../components/Loader';
 const ProductListScreen = () => {
+
+    const { pageNumber } = useParams();
+
+    const { data, isLoading, error, refetch } = useGetProductsQuery({ pageNumber })
+
+    const [deleteProduct, { isLoading: loadingDelete }] = useDeleteProductMutation();
+
+    const deleteHandler = async (id) => {
+        if (window.confirm('Are you sure')) {
+            try {
+                await deleteProduct(id)
+            } catch (err) {
+                toast.error(err?.data?.message || err.error)
+            }
+        }
+    }
+
+    const [createProduct, { isLoading: loadingCreate }] = useCreateProductMutation()
+
+
+
+
     return (
-        <div>ProductListScreen</div>
+        <>
+            <Row className='align-items-center'>
+                <Col>
+                    <h1>Products</h1>
+                </Col>
+                <Col className='text-end'>
+                    <LinkContainer to='/admin/product/create'>
+                        <Button className='my-3 mx-5 bg-slate-400' >
+                            Create Product
+                        </Button>
+                    </LinkContainer>
+                </Col>
+            </Row>
+
+            {loadingCreate && <Loader />}
+            {loadingDelete && <Loader />}
+            {isLoading ? (
+                <Loader />
+            ) : error ? (
+                <Message variant='danger'>{error.data.message}</Message>
+            ) : (
+                <>
+                    <Table striped bordered hover responsive className='table-sm'>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>NAME</th>
+                                <th>PRICE</th>
+                                <th>CATEGORY</th>
+                                <th>BRAND</th>
+                                <th>EDIT/DELETE</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {data.products.map((product) => (
+                                <tr key={product._id}>
+                                    <td>{product._id}</td>
+                                    <td>{product.name}</td>
+                                    <td>${product.price}</td>
+                                    <td>{product.category}</td>
+                                    <td>{product.brand}</td>
+                                    <td>
+                                        <LinkContainer to={`/admin/product/${product._id}/edit`}>
+                                            <Button variant='light' className='btn-sm mx-2'>
+                                                <FaEdit />
+                                            </Button>
+                                        </LinkContainer>
+                                        <Button
+                                            variant='danger'
+                                            className='btn-sm bg-red-400'
+                                            onClick={() => deleteHandler(product._id)}
+                                        >
+                                            <FaTrash style={{ color: 'white' }} />
+                                        </Button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </Table>
+                    <Paginate pages={data.pages} page={data.page} isAdmin={true} />
+                </>
+            )}
+        </>
     )
 }
 
