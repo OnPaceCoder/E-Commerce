4fbea740f9c82624e18c0ad80bc3e8ca2bb240aa
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-10-07 18:32:37 +0530

    Add: CreateProduct screen added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -25,6 +25,7 @@ import UserListScreen from './screens/admin/UserListScreen';
 import ProductEditScreen from './screens/admin/ProductEditScreen';
 import ProductListScreen from './screens/admin/ProductListScreen';
 import UserEditScreen from './screens/admin/UserEditScreen';
+import CreateProductScreen from './screens/admin/CreateProductScreen';
 
 
 
@@ -56,6 +57,7 @@ const router = createBrowserRouter(
         <Route path='/admin/productlist' element={<ProductListScreen />} />
         <Route path='/admin/productlist/:pageNumber' element={<ProductListScreen />} />
         <Route path='/admin/userlist' element={<UserListScreen />} />
+        <Route path='/admin/product/create' element={<CreateProductScreen />} />
         <Route path='/admin/product/:id/edit' element={<ProductEditScreen />} />
         <Route path='/admin/user/:id/edit' element={<UserEditScreen />} />
       </Route>
